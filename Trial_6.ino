6082d5228041c9202c5d7c45562c716a56c7a1ba
Dhruv Sadrawala <115005984+Dhruv-Sadrawala@users.noreply.github.com>
2024-01-18 22:52:54 +0530

    Update Trial_6.ino

--- Trial_6.ino
+++ Trial_6.ino
@@ -18,14 +18,27 @@
 #define YM 9
 #define XP 8
 
-#define BLACK   0x0000
-#define BLUE    0x001F
+#define BLACK   0x0
+#define LIGHT_RED	0xFD14
 #define RED     0xF800
+#define DARK_RED    0x6000
+#define LIGHT_ORANGE 0xFF52
+#define ORANGE  0xFD00
+#define DARK_ORANGE 0xFA80
+#define LIGHT_YELLOW 0xFFF4
+#define YELLOW  0xD7E0
+#define DARK_YELLOW 0xCE40
+#define LIGHT_GREEN 0xB7F6
 #define GREEN   0x07E0
+#define DARK_GREEN 0x0320
+#define LIGHT_BLUE 0xA51F
+#define BLUE    0x001F
+#define DARK_BLUE 0x000C
+#define PURPLE  0xF81F
 #define CYAN    0x07FF
-#define MAGENTA 0xF81F
-#define YELLOW  0xFFFF00
+#define GREY    0x8410
 #define WHITE   0xFFFF
+
 
 Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
 
@@ -33,11 +46,20 @@ TouchScreen ts = TouchScreen(XP, YP, XM, YM, 364);
 
 boolean buttonEnabled = true;
 
-void setup() {
+int roti_co=0;
+int H2O_co=0;
+int dal_co=0;
+int rice_co=0;
 
-  int rectWidth = 300;
-  int rectHeight = 30;  // Menu Items
-  int cornerRadius = 10;
+void setup() {
+  menu();
+
+}
+
+void menu(){
+
+  int rectWidth = 145;
+  int rectHeight = 50;  // Menu Items
   int rectX = 10;
   int rectY = 5;
   
@@ -49,55 +71,48 @@ void setup() {
   tft.drawRect(0,0,319,240,WHITE);
 
   tft.setCursor(20,15);
-  tft.fillRect(rectX, rectY, rectWidth, 50, RED);
+  tft.fillRect(rectX, rectY, 300, 50, RED);
   tft.setTextSize(2);
   tft.setTextColor(WHITE);
   tft.print("SMART MENU ORDERING ON          TABLE (SMOT)");
 
-  tft.fillRect(10, 60, rectWidth, rectHeight, BLUE);
+  tft.fillRect(10, 70, rectWidth, rectHeight, BLUE);
   tft.setTextSize(2);
   tft.setTextColor(WHITE);
-  tft.setCursor(24,68);
-  tft.print("Gujarati Thali Rs.210/-");
+  tft.setCursor(21,86);
+  tft.print("Roti Rs.20");
 
-  tft.fillRect(10, 95, rectWidth, rectHeight, BLUE);
+  tft.fillRect(165, 70, rectWidth, rectHeight, BLUE);
   tft.setTextSize(2);
   tft.setTextColor(WHITE);
-  tft.setCursor(24,103);
-  tft.print("Punjabi Thali  Rs.200/-");
+  tft.setCursor(178,86);
+  tft.print("H2O Rs.15");
 
   tft.fillRect(10, 130, rectWidth, rectHeight, BLUE);
   tft.setTextSize(2);
   tft.setTextColor(WHITE);
-  tft.setCursor(24,138);
-  tft.print("Marwadi Thali  Rs.220/-");
-  
-  
+  tft.setCursor(21,146);
+  tft.print("Dal Rs.100");
 
-  /*tft.setTextColor(BLACK);
+  tft.fillRect(165, 130, rectWidth, rectHeight, BLUE);
   tft.setTextSize(2);
-  tft.fillScreen(WHITE);
-  tft.drawRect(10, 60, 300, rectHeight, BLUE);
-  tft.print("Gujarati Thali Rs.210/-");
-
-  tft.setTextColor(BLACK);
-  tft.setTextSize(2);
-  tft.fillScreen(WHITE);
-  tft.drawRect(10, 95, 300, rectHeight, BLUE);
-  tft.print("Punjabi Thali Rs.200/-");
-  
-  tft.setTextColor(BLACK);
-  tft.setTextSize(2);
-  tft.fillScreen(WHITE);
-  tft.drawRect(10, 130, 300, rectHeight, BLUE);
-  tft.print("Marwadi Thali Rs.220/-");
-  
-  /*tft.fillRect(50,180, 210, 40, RED);
-  tft.drawRect(50,180,210,40,BLACK);
-  tft.setCursor(60,190);
   tft.setTextColor(WHITE);
+  tft.setCursor(172,146);
+  tft.print("Rice Rs.120");
+
+  tft.fillRect(10,185, 300, 45, LIGHT_ORANGE);
   tft.setTextSize(2);
-  tft.print("Please Subscribe");*/
+  tft.setTextColor(WHITE);
+  tft.setCursor(90,199);
+  tft.print("Review Order");
+}
+
+void review(){
+  tft.fillRect(165, 70, 70, 50, BLUE);
+  tft.setTextSize(2);
+  tft.setTextColor(BLACK);
+  tft.setCursor(178,86);
+  tft.print("H2O Rs.15");
 
 }
 
@@ -109,57 +124,32 @@ void loop() {
    p.x = map(p.x, TS_MAXX, TS_MINX, 0, 320);
    p.y = map(p.y, TS_MAXY, TS_MINY, 0, 480);
        
-   if(p.x>10 && p.x<300 && p.y>60 && p.y<90 && buttonEnabled){
+   if(p.x>10 && p.x<155 && p.y>50 && p.y<120 && buttonEnabled){
     
-    buttonEnabled = false;
-    
-    pinMode(XM, OUTPUT);
-    pinMode(YP, OUTPUT);
-    
-    tft.fillScreen(WHITE);
-    tft.setCursor(100,150);
-    tft.setTextColor(BLACK);
-    tft.setTextSize(2);
-    tft.print(" You have\n\n       ordered\n\n     Gujarati Thali");
+    roti_co++;
    }
 
-   else if(p.x>10 && p.x<300 && p.y>98 && p.y<128 && buttonEnabled){
+   else if(p.x>165 && p.x<310 && p.y>50 && p.y<120 && buttonEnabled){
     
-    buttonEnabled = false;
-    
-    pinMode(XM, OUTPUT);
-    pinMode(YP, OUTPUT);
-    
-    tft.fillScreen(WHITE);
-    tft.setCursor(100,150);
-    tft.setTextColor(BLACK);
-    tft.setTextSize(2);
-    tft.print(" You have\n\n       ordered\n\n     Punjabi Thali");
+    H2O_co++;
    }  
+  
+  else if(p.x>10 && p.x<155 && p.y>130 && p.y<180 && buttonEnabled){
+    
+    dal_co++;
   }
 
-  else if(p.x>10 && p.x<300 && p.y>129 && p.y<215 && buttonEnabled){
+  else if(p.x>10 && p.x<310 && p.y>130 && p.y<180 && buttonEnabled){
     
-    buttonEnabled = false;
-    
-    pinMode(XM, OUTPUT);
-    pinMode(YP, OUTPUT);
-    
-    tft.fillScreen(WHITE);
-    tft.setCursor(100,150);
-    tft.setTextColor(BLACK);
-    tft.setTextSize(2);
-    tft.print(" You have\n\n       ordered\n\n     Marwadi Thali");
-
-  /*tft.fillRect(200, 60, 30, 40, BLUE);
-  tft.setTextSize(2);
-  tft.setTextColor(WHITE);
-  tft.setCursor(24,68);
-  tft.print("BACK");
-
-   if(p.x>200 && p.x<240 && p.y>60 && p.y<90 && buttonEnabled)
-    setup();
-   }  
-*/
+    rice_co++;
   }
+
+  else if(p.x>10 && p.x<310 && p.y>175 && p.y<225 && buttonEnabled){
+    
+    review();
+    delay(70000);
+
+  }
+ }
+
 }
